f8913f44ebb2cf9bf00ddffd0ad0a1258f8e1d7f
XMG-II\fuhax <ilja.immler@gmail.com>
2018-11-17 22:06:28 +0100

    pshu

--- Assets/Scripts/Room/Room.cs
+++ Assets/Scripts/Room/Room.cs
@@ -4,19 +4,17 @@ using Sirenix.Serialization;
 using UnityEngine;
 
 public class Room : MonoBehaviour
-{
-    public static List<Room> rooms = new List<Room>();
-    
+{   
     public float temperature;
     public float pressure;
 
     private void OnEnable()
     {
-        rooms.Add(this);
+        RoomManager.Add(this);
     }
 
     private void OnDisable()
     {
-        rooms.Remove(this);
+        RoomManager.Remove(this);
     }
 }
