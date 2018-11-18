6a5cae5059c77769f03ce6eac11bb52182509a06
XMG-II\fuhax <ilja.immler@gmail.com>
2018-11-18 09:56:10 +0100

    fck

---
+++ Assets/FunctionKeyToggle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Experimental.XR.Interaction;
+using UnityEngine.SceneManagement;
+
+public class FunctionKeyScenario : MonoBehaviour
+{
+    public KeyCode code;
+    public int scenario;
+
+    private bool loaded = false;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(code))
+        {
+            if (loaded)
+            {
+                SceneManager.UnloadSceneAsync(scenario);
+            }
+            else
+            {
+                SceneManager.LoadSceneAsync(scenario);
+            }
+        }    
+    }
+}
