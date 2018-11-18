fb9be1b8cfc08e372f7bb002fcf8c03c0a6e1047
Wuselwog <philngbr@googlemail.com>
2018-11-18 03:09:58 +0100

    Added Sensors and Consumers

---
+++ Assets/Scripts/IoT/IoTConsumerTVSleepRoom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IoTConsumerTVSleepRoom : IoTConsumer
+{
+    public override bool IsEventMatch(IoTEvent e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Consume(IoTEvent e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
